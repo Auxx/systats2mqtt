bd306ac3e0e836cda049c45277835e3618a95f0d
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2022-01-16 15:52:01 +0000

    Project setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ScheduleModule.forRoot(),
+    SchedulerModule
+  ],
+  controllers: [ AppController ],
+  providers: [ AppService ]
 })
-export class AppModule {}
+export class AppModule {
+}
