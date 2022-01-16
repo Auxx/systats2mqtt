e1df900a5b9f402e0aad8b7d2a151d5061967b67
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2022-01-16 16:09:52 +0000

    SystemMonitorService stub

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { SystemMonitorService } from './system-monitor/system-monitor.service';
 
-@Module({})
+@Module({
+  providers: [SystemMonitorService]
+})
 export class SchedulerModule {}
---
+++ src/scheduler/system-monitor/system-monitor.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { Interval } from '@nestjs/schedule';
+
+import { battery, cpu, cpuTemperature, currentLoad, fsSize, mem } from 'systeminformation';
+
+@Injectable()
+export class SystemMonitorService {
+  constructor() {
+    this.loadSystemInfo();
+  }
+
+  @Interval(5000)
+  test() {
+    console.log('xxx');
+
+    cpuTemperature().then(info => console.log(info));
+    // mem().then(info => console.log(info));
+    // battery().then(info => console.log(info));
+    // currentLoad().then(info => console.log(info));
+    // fsSize().then(info => console.log(info));
+  }
+
+  loadSystemInfo() {
+    cpu().then(info => console.log(info));
+  }
+}
